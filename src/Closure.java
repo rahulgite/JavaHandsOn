a1e9d123dbae0cb01fa5a0bb2223f148aa75dcdc
RG <imgite@gmail.com>
2022-01-29 20:01:16 +0530

    Closure

---
+++ src/Closure.java
@@ -0,0 +1,32 @@
+
+    @FunctionalInterface
+    interface Operation
+    {
+        void operate(int n);
+    }
+
+    public class Closure
+    {
+        public static void main(String args[])
+        {
+            int x=20;
+            int y=90;
+            //the method parses two parameters i.e. x and the operate() method of the Operation interface
+            //implementation of closure in lambda expression
+            doSum(x, new Operation()
+            {
+                //overrides the operate() method
+                @Override
+                public void operate(int n)
+                {
+                    //prints the result
+                    System.out.println("Sum is: "+(n+y));
+                }
+            });
+        }
+        private static void doSum(int i, Operation op)
+        {
+            op.operate(i);
+        }
+    }
+
