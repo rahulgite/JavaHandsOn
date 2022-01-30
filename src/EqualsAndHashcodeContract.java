d964c30a0a0e4f0591f00990a7a9eb63d78af817
RG <imgite@gmail.com>
2022-01-30 18:00:20 +0530

    Updates

---
+++ src/EqualsAndHashcodeContract.java
@@ -0,0 +1,55 @@
+import java.util.Objects;
+
+public class EqualsAndHashcodeContract {
+    public static void main(String[] args) {
+        Employee e1=new Employee();
+        e1.setId(1);
+        e1.setName("RG");
+        Employee e2=new Employee();
+        e2.setId(1);
+        e2.setName("RG");
+
+        System.out.println(e1.hashCode() +" "+e2.hashCode());
+        System.out.println("Shallow Comparison: "+ (e1==e2));
+        System.out.println("Deep Comparison: "+ (e1.equals(e2)));
+    }
+}
+
+class Employee{
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Employee)) return false;
+        Employee employee = (Employee) o;
+        return Objects.equals(getId(), employee.getId()) && Objects.equals(getName(), employee.getName());
+    }
+
+    public int hashCode() {
+        int result = 17;
+        if (name != null) {
+            result = 31 * result + name.hashCode();
+        }
+        if (id != null) {
+            result = 31 * result + id.hashCode();
+        }
+        return result;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    Integer id;
+    String name;
+}
---
+++ src/SynchronizedExamples.java
@@ -0,0 +1,60 @@
+class Table
+{
+    int balance=1000;
+     void withdraw(){
+        synchronized(this){//synchronized block
+            for(int i=1;i<=5;i++){
+                System.out.println(Thread.currentThread().getName());
+                System.out.println(balance-=i);
+                try{
+                    Thread.sleep(400);
+                }catch(Exception e){System.out.println(e);}
+            }
+        }
+
+    }//end of the method
+    void add(){
+        synchronized(this){//synchronized block
+            for(int i=1;i<=5;i++){
+                System.out.println(Thread.currentThread().getName());
+                System.out.println(balance+=200);
+                try{
+                    Thread.sleep(400);
+                }catch(Exception e){System.out.println(e);}
+            }
+        }
+    }
+}
+
+class MyThread1 extends Thread{
+    Table t;
+    MyThread1(Table t){
+        this.t=t;
+    }
+    public void run(){
+        t.withdraw();
+    }
+
+}
+class MyThread2 extends Thread{
+    Table t;
+    MyThread2(Table t){
+        this.t=t;
+    }
+    public void run(){
+        t.add();
+    }
+}
+
+public class SynchronizedExamples{
+    public static void main(String args[]){
+        Table obj = new Table();//only one object
+        MyThread1 t1=new MyThread1(obj);
+        MyThread2 t2=new MyThread2(obj);
+        t1.start();
+        t2.start();
+        //System.out.println("Closing Balance"+obj.balance);
+    }
+}
+
+
